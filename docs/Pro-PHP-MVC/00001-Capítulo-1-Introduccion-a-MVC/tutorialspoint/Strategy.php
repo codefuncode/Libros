38941472539ccac1275ba77dbdec522d343aa3e5
Carlos Aleman <alemandiazcarlos@protonmail.com>
2021-05-06 23:53:40 -0400

    comentarios

--- docs/Pro-PHP-MVC/00001-Capítulo-1-Introduccion-a-MVC/tutorialspoint/Strategy.php
+++ docs/Pro-PHP-MVC/00001-Capítulo-1-Introduccion-a-MVC/tutorialspoint/Strategy.php
@@ -1,5 +1,6 @@
 <?php
-
+// ========================================================
+//  Dos clases que se pasaran por parámetro a la clase principal
 class IdComparator
 {
 
@@ -43,26 +44,32 @@ class DateComparator
         return $his->resultado;
     }
 }
+// ========================================================
 
+//  No hay herencia la obtiene la instancia por medio del parámetro
 class ObjectCollection
 {
+    //  Variables para trabajar
     public $instancia;
     public $matriz;
     public $resultado;
 
     public function __construct($p_matriz)
     {
+        //  recoger matriz
         $this->matriz = $p_matriz;
     }
 
     public function setComparator($p_class)
     {
-
+        // Establecer la instancia de la clase recibida por parámetro
         $this->instancia = $p_class;
 
     }
     public function sort()
     {
+
+        //  Retornara lo que retornara case reciba de la case de turno
 
         $this->instancia($this->matriz);
         $his->resultado = $this->instancia->getComparacion();
@@ -85,16 +92,54 @@ echo $elements[0]["id"];
 echo '<br/>';
 echo $elements[1]["id"];
 
+// -
+//  creando la instancia
 $collection = new ObjectCollection($elements);
+// -
 
+//  Usando metodo de la instancia
 $collection->setComparator(new IdComparator());
 $collection->sort();
 
+//  salida
 echo "Sorted by ID:\n";
 print_r($collection->elements);
 
+//  Usando metodo de la instancia
 $collection->setComparator(new DateComparator());
 $collection->sort();
 
+//  salida
 echo "Sorted by date:\n";
 print_r($collection->elements);
+
+// ===================================================================
+// ===================================================================
+//  Ejemplo origuinal en https://www.tutorialspoint.com/php/php_design_patterns.htm
+
+// $elements = array(
+//      array(
+//         'id' => 2,
+//         'date' => '2011-01-01',
+//      ),
+//      array(
+//         'id' => 1,
+//         'date' => '2011-02-01'
+//      )
+//   );
+
+//   $collection = new ObjectCollection($elements);
+
+//   $collection->setComparator(new IdComparator());
+//   $collection->sort();
+
+//   echo "Sorted by ID:\n";
+//   print_r($collection->elements);
+
+//   $collection->setComparator(new DateComparator());
+//   $collection->sort();
+
+//   echo "Sorted by date:\n";
+//   print_r($collection->elements);
+// ===================================================================
+// ===================================================================
